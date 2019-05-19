60d56eb26f283dd12635b148845c829052c21cb5
Quynh Nguyen <quynhanh87@gmail.com>
2019-05-18 17:28:00 -0700

    Add I Am Rich app

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Quynh Nguyen on 5/4/19.
+//  Copyright © 2019 Quynh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
