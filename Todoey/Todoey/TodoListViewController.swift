f5c2dc0e4be235ae067bdf79df54ea2408eeda47
Quynh Nguyen <quynhanh87@gmail.com>
2019-06-01 12:49:21 -0700

    Todoey: Add search bar

--- Todoey/Todoey/TodoListViewController.swift
+++ Todoey/Todoey/TodoListViewController.swift
@@ -40,9 +40,9 @@ class TodoListViewController: UITableViewController {
         // To delete item
         //context.delete(itemArray[indexPath.row])
         //itemArray.remove(at: indexPath.row)
-        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         saveItems()
+        
         // TODO : reload row instead of table
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -80,12 +80,33 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request : NSFetchRequest<Item> = Item.fetchRequest()) {
         do {
             itemArray = try context.fetch(request)
         } catch {
             print("Error fetching data from context: \(error)")
+        }
+        tableView.reloadData()
+    }
+}
+
+//MARK: - Search bar methods
+
+extension TodoListViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
         }
     }
 }
