7c4f7517c2ccfe50d2a2071f1c59500058c579e7
Quynh Nguyen <quynhanh87@gmail.com>
2019-06-14 15:43:22 -0700

    Joke Bank: Add view controller to display joke

--- Joke Bank/Joke Bank/JokeTableViewController.swift
+++ Joke Bank/Joke Bank/JokeTableViewController.swift
@@ -10,87 +10,48 @@ import UIKit
 
 class JokeTableViewController: UITableViewController {
     
-    var jokesCategory = ["Chicken", "Walk in a bar", "Olive", "Racer"]
+    var jokes = ["Chicken", "Doctor", "Lawyer"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
-    /*
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-    */
 
+    // Determine how many row to display
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return jokesCategory.count
+        return jokes.count
     }
 
-    
+    // Define what is displayed inside cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Category", for: indexPath)
-        
-        cell.textLabel?.text = jokesCategory[indexPath.row]
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "Category", for: indexPath)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = jokes[indexPath.row]
 
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "goToJoke", sender: self)
+        let selectedJoke = jokes[indexPath.row]
+        performSegue(withIdentifier: "goToJoke", sender: selectedJoke)
     }
- 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
         // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        //let destinationVC = segue.destination as! JokeViewController
+        if let jokeVC = segue.destination as? JokeViewController {
+            
+            if let selectedJoke = sender as? String {
+                jokeVC.joke = selectedJoke
+            }
+            //if let indexPath = tableView.indexPathForSelectedRow?.item {
+            //    jokeVC.title = jokes[indexPath]
+            //}
+        }
     }
-    */
-
 }
