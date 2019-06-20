3cb1f17cb154cec552a912f52ba44c0ebc7c0331
Quynh Nguyen <quynhanh87@gmail.com>
2019-06-20 14:37:15 -0700

    Calculator: User can input number and number show on display

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    var result: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func numBtnPressed(_ sender: UIButton) {
+        
+        if let userInput = sender.currentTitle {
+            result = (result == "") ? userInput : (result + userInput)
+            answerLabel.text = result
+        }
+    }
 }
 
