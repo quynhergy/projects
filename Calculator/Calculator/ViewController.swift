eccb03849cbf72237c04002bc4407f032d1707c2
Quynh Nguyen <quynhanh87@gmail.com>
2019-06-20 15:32:17 -0700

    Calculator: Add clear, percentage, and +/- action function.

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var displayLabel: UILabel!
     
-    var result: String = ""
+    private var isFinishedTyping: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,11 +19,31 @@ class ViewController: UIViewController {
     }
     
     @IBAction func numBtnPressed(_ sender: UIButton) {
-        
         if let userInput = sender.currentTitle {
-            result = (result == "") ? userInput : (result + userInput)
-            answerLabel.text = result
+            if isFinishedTyping {
+                displayLabel.text = userInput
+                isFinishedTyping = false
+            } else {
+                displayLabel.text = displayLabel.text! + userInput
+            }
+        }
+    }
+    
+    @IBAction func calcBtnPressed(_ sender: UIButton) {
+        isFinishedTyping = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a double.")
+        }
+        
+        if let actionBtn = sender.currentTitle {
+            if actionBtn == "%" {
+                displayLabel.text = String(number * 0.01)
+            } else if actionBtn == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if actionBtn == "AC" {
+                displayLabel.text = "0"
+            }
         }
     }
 }
-
