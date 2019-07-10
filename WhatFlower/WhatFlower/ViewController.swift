ab41acd77338fb8f671b36469d016f5f08c56bb4
Quynh Nguyen <quynhanh87@gmail.com>
2019-07-10 15:23:50 -0700

    WhatFlower: Update app to retrieve wikipedia info

--- WhatFlower/WhatFlower/ViewController.swift
+++ WhatFlower/WhatFlower/ViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 import CoreML
 import Vision
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
     
     private let imagePicker = UIImagePickerController()
+    private let wikipediaURL = "https://en.wikipedia.org/w/api.php"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,8 +51,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
         
         let request = VNCoreMLRequest(model: model) { (request, error) in
-            let classification = request.results?.first as? VNClassificationObservation
-            self.navigationItem.title = classification?.identifier.capitalized
+            guard let classification = request.results?.first as? VNClassificationObservation else {
+                fatalError("Could not classify image.")
+            }
+            self.requestInfo(flowerName: classification.identifier)
+            self.navigationItem.title = classification.identifier.capitalized
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
@@ -57,6 +64,32 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             try handler.perform([request])
         } catch {
             print(error)
+        }
+    }
+    
+    private func requestInfo(flowerName: String) {
+        
+        let parameters : [String:String] = [
+            "format" : "json",
+            "action" : "query",
+            "prop" : "extracts",
+            "exintro" : "",
+            "explaintext" : "",
+            "titles" : flowerName,
+            "indexpageids" : "",
+            "redirects" : "1",
+        ]
+        
+        Alamofire.request(wikipediaURL, method: .get, parameters: parameters).responseJSON { (response) in
+            if response.result.isSuccess {
+                print("Success! Got the wikipedia info.")
+                let wikiJSON : JSON = JSON(response.result.value!)
+                let pageid = wikiJSON["query"]["pageids"][0].stringValue
+                let description = wikiJSON["query"]["pages"][pageid]["extract"].stringValue
+                self.label.text = description
+            } else {
+                print("Error: \(String(describing: response.result.error))")
+            }
         }
     }
 }
