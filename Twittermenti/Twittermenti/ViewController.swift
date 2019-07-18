9859e3af72df79b87be8f46105d081d2f7e44020
Quynh Nguyen <quynhanh87@gmail.com>
2019-07-18 13:48:14 -0700

    Twittermenti: Add base project

---
+++ Twittermenti/Twittermenti/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by Quynh Nguyen on 7/17/19.
+//  Copyright © 2019 Quynh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var sentimentLabel: UILabel!
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func predictPressed(_ sender: Any) {
+    }
+    
+}
+
