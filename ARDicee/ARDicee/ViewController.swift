06711301d78e1011cfe5e5300da59abf07540d0d
Quynh Nguyen <quynhanh87@gmail.com>
2019-07-22 00:10:17 -0700

    ARDicee: Start project and add dice to scene

---
+++ ARDicee/ARDicee/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  ARDicee
+//
+//  Created by Quynh Nguyen on 7/21/19.
+//  Copyright © 2019 Quynh Nguyen. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Set the scene to display a dice
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+            sceneView.scene.rootNode.addChildNode(diceNode)
+            sceneView.autoenablesDefaultLighting = true
+        }
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration.isSupported ? ARWorldTrackingConfiguration() : AROrientationTrackingConfiguration()
+        //let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+}
